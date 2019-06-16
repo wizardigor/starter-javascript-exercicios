e1f69bc0cfad4ef172733e16119cacd847561c61
wizardigor <51889513+wizardigor@users.noreply.github.com>
2019-06-16 18:26:01 -0300

    Update 04.js
    Fiz uma delimitação entre os valores.

--- Módulo 01/04.js
+++ Módulo 01/04.js
@@ -1,9 +1,9 @@
 function experiencia(anos) {
   if (anos <= 1) {
     return "Iniciante";
-  } else if (anos <= 3) {
+  } else if ((anos > 1) && (anos <=3)) {
     return "Intermediário";
-  } else if (anos <= 6) {
+  } else if ((anos > 3) && (anos <= 6)) {
     return "Avançado";
   } else {
     return "Jedi Master";
